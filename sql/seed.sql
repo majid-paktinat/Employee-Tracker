f5ebc550d47cb979142fd0c45568cd2d84d34879
majid <majid.paktinat@gmail.com>
2020-03-08 20:26:15 -0400

    seed updated

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,21 +1,28 @@
 use employeetracker;
 
-insert into employee(first_name, last_name, role_id, manager_id) values("Jared", "Deckar", 1,1);
-insert into employee(first_name, last_name, role_id, manager_id) values("Tamer", "Gala", 2,3);
-insert into employee(first_name, last_name, role_id, manager_id) values("Alec", "Dier", 4,2);
-insert into employee(first_name, last_name, role_id, manager_id) values("Don", "Green", 3,4);
-insert into employee(first_name, last_name, role_id, manager_id) values("Steve", "Smith", 5,1);
+insert into employee(first_name, last_name, role_id, manager_id) values("Jackie", "Deckar", 17,null);
+insert into employee(first_name, last_name, role_id, manager_id) values("Tamer", "Gala", 16,39);
+insert into employee(first_name, last_name, role_id, manager_id) values("Alec", "Dier", 18,40);
+insert into employee(first_name, last_name, role_id, manager_id) values("Don", "Green", 21,39);
+insert into employee(first_name, last_name, role_id, manager_id) values("Steve", "Smith", 22,44);
+insert into employee(first_name, last_name, role_id, manager_id) values("Melanie", "Gilbert", 22,39);
 
 
-insert into role(title, salary, department_id) values("team lead", "100", 1);
-insert into role(title, salary, department_id) values("business analyst", "80", 2);
-insert into role(title, salary, department_id) values("juior developer", "70", 1);
-insert into role(title, salary, department_id) values("DBA", "90", 1);
-insert into role(title, salary, department_id) values("senior developer", "90", 1);
+
+
+insert into role(title, salary, department_id) values("Web Development - Team Lead", "100", 9);
+insert into role(title, salary, department_id) values("Business Analyst", "80", 10);
+insert into role(title, salary, department_id) values("Juior Developer", "70", 9);
+insert into role(title, salary, department_id) values("Finance Specialist", "85", 13);
+insert into role(title, salary, department_id) values("Customer Service Agent", "55", 14);
+insert into role(title, salary, department_id) values("Region Sales Specialist", "70", 11);
+insert into role(title, salary, department_id) values("Branding Specialist", "65", 10);
+
 
 
 insert into department(name) values("IT")
 insert into department(name) values("Marketing")
 insert into department(name) values("Sales")
 insert into department(name) values("Procurement")
+insert into department(name) values("Finance")
 insert into department(name) values("Customer Relations")
